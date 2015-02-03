055496821fd08baaf236793bb2d94675fdd7e09d
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-02-03 11:01:38 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloApp::Application.config.secret_token = '52d17ce27f95ab3557c7f1c952eac44b08172c95c952ae57244c4f784fd6773dfe720fba7bb6662af963a03643b7fd71df51056a1ce451e50e8755da18e9a4d9'
